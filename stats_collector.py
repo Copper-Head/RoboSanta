7ef55c8547651ddd1167f9fae4e39c0fc7071037
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2017-08-30 16:39:23 +0200

    Cleaned up stats_collector

--- stats_collector.py
+++ stats_collector.py
@@ -1,7 +1,4 @@
-"""Kinda silly example of how to collect stats from running multiple instances.
-
-Runs instances from an asprilo folder and produces a file called "test.csv" with some data about the runs.
-"""
+"""Tries to solve multiple instances and collects stats from the runs. """
 from itertools import product, starmap
 import os
 import json
@@ -14,7 +11,6 @@ from robosanta import Solver, load_files
 from fileIO import read_json, experiment_item_name
 
 CONFIGS_ROOT = "configs"
-
 PHILLIP_INSTANCES = "/home/quickbeam/aspilro-instances/phillip"
 TOPOLOGY_INSTANCES = "/home/quickbeam/aspilro-instances/topologies"
 STATS_BASE_DIR = '/home/quickbeam/aspilro-instances/topologies/stats'
@@ -23,6 +19,7 @@ ExperimentalItem = namedtuple("ExperimentalItem", "name filepath config")
 
 
 def single_solver(item: ExperimentalItem):
+    """Sets up Solver instance, runs it, returns it."""
     modules = item.config['modules_stage_one'] + item.config['modules_stage_two']
     solver = Solver(item.filepath, *modules, verbose=False)
     solver.solve_normal()
@@ -30,20 +27,23 @@ def single_solver(item: ExperimentalItem):
 
 
 def combine_exp_data(config, instance_path):
+    """Merges various information to produce an ExperimentalItem instance."""
     instance_name = experiment_item_name(instance_path)
     return ExperimentalItem(instance_name, instance_path, config)
 
 
 def track_progress(iterable):
+    """Lets tqdm show a progress bar for the iterable."""
     return tqdm(list(iterable))
 
 
 def get_stats(slvr: Solver):
+    """Wrapper for accessing the statistics attribute of a Solver instance."""
     return slvr.control.statistics
 
 
-def write_experiment_stats(filename, stats):
-    with open(filename, mode='w') as fhandle:
+def write_experiment_stats(filepath: str, stats: dict):
+    with open(filepath, mode='w') as fhandle:
         json.dump(stats, fhandle, indent=2)
 
 
@@ -55,15 +55,15 @@ def stats_outpath(stats_base_dir: str, item: ExperimentalItem, ext=".stats"):
 def main():
 
     all_configs = ["2-robots-ta.json",]
-
     configs = map(read_json, (os.path.join(CONFIGS_ROOT, fname) for fname in all_configs))
-
     all_paths = load_files(os.path.join(TOPOLOGY_INSTANCES, 'tiny_cases'))
+
     items = track_progress(starmap(combine_exp_data, product(configs, all_paths)))
+    # consider simplifying this?
     pipeline = juxt(stats_outpath(STATS_BASE_DIR), compose(get_stats, single_solver))
 
-    for _ in starmap(write_experiment_stats, map(pipeline, items)):
-        pass
+    for fpath, stats in map(pipeline, items):
+        write_experiment_stats(fpath, stats)
 
 
 if __name__ == '__main__':
