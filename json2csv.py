fe23d0e0ef2baade0250236674a7c352efb7d1ef
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2017-08-31 08:19:15 +0200

    Module docstring, more getters

--- json2csv.py
+++ json2csv.py
@@ -1,48 +1,74 @@
-from collections import OrderedDict
+"""Turns a collection of stats JSON files into a CSV.
+
+The point of separating this that collecting all the stats is potentially costly,
+so it's better to do that once, get as much as possible and store that,
+then extract specific information from them into CSV files taylored to a specific purpose.
+"""
+from collections import OrderedDict, namedtuple
 import csv
+import re
 
 from toolz import curry
 
 from fileIO import experiment_item_name, iterate_dir, read_json
 from stats_collector import STATS_BASE_DIR
 
-def _get_time(stats: dict, key: str):
-    return stats['summary']['times'][key]
+ExperimentData = namedtuple("ExperimentData", "name summary")
 
 
-def get_solve_time(stats: dict):
-    return _get_time(stats, 'solve')
+def parse_experiment_data(stats_path: str):
+    stats = read_json(stats_path)
+    return ExperimentData(experiment_item_name(stats_path), stats['summary'])
 
 
-def get_total_time(stats: dict):
-    return _get_time(stats, 'total')
+def get_grid_size(data: ExperimentData):
+    m = re.search("_n(\d+)_", data.name)
+    return m.group(1) if m else 0
 
 
-def get_solved(stats: dict):
-    return stats['summary']['result'] == 1.0
-
-def get_makespan(stats: dict):
-    return stats['summary']['costs'][0]
+def _get_time(data: ExperimentData, key: str):
+    return data.summary['times'][key]
 
 
+def get_solve_time(data: ExperimentData):
+    return _get_time(data, 'solve')
+
+
+def get_total_time(data: ExperimentData):
+    return _get_time(data, 'total')
+
+
+def get_solved(data: ExperimentData):
+    return data.summary['result'] == 1.0 and get_solve_time(data) > 0.0
+
+
+def get_makespan(data: ExperimentData):
+    return data.summary['costs'][0]
+
+
+# yapf: disable
 ENTRIES = OrderedDict([
-    # (''),
+    ('name', lambda data: data.name),
+    ('grid_size', get_grid_size),
     ('solved', get_solved),
     ('total_time', get_total_time),
     ('solve_time', get_solve_time),
     ('plan_makespan', get_makespan)])
+# yapf: enable
+
 
 @curry
-def create_row(fields: OrderedDict, json_stats: dict):
-    return dict((field_name, field_getter(json_stats)) for field_name, field_getter in fields.items())
-
+def create_row(fields: OrderedDict, data: ExperimentData):
+    return dict((field_name, field_getter(data)) for field_name, field_getter in fields.items())
 
 
 def main():
     with open('stats-new.csv', mode='w') as fh:
         writer = csv.DictWriter(fh, fieldnames=ENTRIES.keys())
         writer.writeheader()
-        writer.writerows(map(create_row(ENTRIES), map(read_json, iterate_dir(STATS_BASE_DIR))))
+        writer.writerows(
+            map(create_row(ENTRIES), map(parse_experiment_data, iterate_dir(STATS_BASE_DIR))))
+
 
 if __name__ == '__main__':
     main()
